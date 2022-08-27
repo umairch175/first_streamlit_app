e5b24953192eb598b722fc758e291dab17477794
Ahmad Umair Choudhry <111965269+umairch175@users.noreply.github.com>
2022-08-27 09:25:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding', add_my_fruit)
-#streamlit.text('Thanks for adding Jackfruit')
+#streamlit.write('Thanks for adding', add_my_fruit)
+streamlit.text('Thanks for adding Jackfruit')
+
+#Thi will not work correctly, but jsut go with it for now
+my_cur.execute ("insert into fruit_load_list values ( 'from streamlist')")
