0c017ecc00b680a3acfad896bbd2d9b945e8b778
Ahmad Umair Choudhry <111965269+umairch175@users.noreply.github.com>
2022-08-28 02:30:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 #import pandas
-#import requests
+import requests
 #import snowflake.connector
 #from urllib.error import URLERROR
 
@@ -25,18 +25,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-import requests
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+        streamlit.error ("Please select a fruit to get information.")
+  else:   
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text(fruityvice_response.json())
- # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+except UTLError as e:
+     streamlit.error()
+#streamlit.text(fruityvice_response.json())
+
+
 
 streamlit.stop()
 
