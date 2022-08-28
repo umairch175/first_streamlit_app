b3a0d0425774ac3b216131fe8b2f2e59c6d1ee2e
Ahmad Umair Choudhry <111965269+umairch175@users.noreply.github.com>
2022-08-28 02:42:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-#import pandas
-
-#import snowflake.connector
+import pandas
+import requests
+import snowflake.connector
 from urllib.error import URLERROR
 
 streamlit.title ('My Parents New Healthy Diner')
