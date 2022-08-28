e59898277114f1be6736459842c5785e207bf69a
Ahmad Umair Choudhry <111965269+umairch175@users.noreply.github.com>
2022-08-28 02:58:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-except URLError as e:
-     streamlit.error()
+#except URLError as e:
+ #    streamlit.error()
 #streamlit.text(fruityvice_response.json())
 
 
